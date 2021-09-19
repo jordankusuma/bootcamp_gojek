d1b23701a50c390917c0f6ff0a6f4d7d788d1c41
jordankusuma <c14180102@john.petra.ac.id>
2021-09-19 11:07:39 +0700

    add setter and getter, and input for Cell array

--- Conway.java
+++ Conway.java
@@ -1,2 +1,32 @@
-package com.company;public class Conway {
+package com.company;
+import java.util.Scanner;
+
+public class Conway {
+    private NewCell[][] array;
+    private NewCell[][] answer;
+    private int turn;
+
+    public Conway(NewCell cell, int turn){
+        NewCell[][] array = new NewCell[cell.getLength()][cell.getWidth()];
+        NewCell[][] answer = new NewCell[cell.getLength()][cell.getWidth()];
+        this.turn = turn;
+    }
+
+    public void setArrayValues(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter numbers between 1 and 0 for the array: ");
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 12; j++) {
+                int input = sc.nextInt();
+                while (input != 0 && input != 1) {
+                    System.out.println("Enter numbers again: ");
+                    input = sc.nextInt();
+                }
+                array[i][j].setArraywithIndex(i, j, input);
+            }
+            System.out.println("");
+        }
+    }
+
+    
 }
--- NewCell.java
+++ NewCell.java
@@ -1,2 +1,47 @@
-package com.company;public class NewCell {
+package com.company;
+
+public class NewCell {
+    private int length;
+    private int width;
+    private int[][] array;
+    private int[][] answer;
+
+    public NewCell(int length, int width){
+        this.length = length;
+        this.width = width;
+        this.array = new int[length][width];
+        this.answer = new int[length][width];
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int[][] getArray() {
+        return array;
+    }
+
+    public void setArraywithIndex(int posX, int posY, int value) {
+        this.array[posX][posY] = value;
+    }
+
+    public int[][] getAnswer() {
+        return answer;
+    }
+
+    public void setAnswerwithIndex(int posX, int posY, int value) {
+        this.answer[posX][posY] = value;
+    }
 }
